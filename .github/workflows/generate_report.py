af334bad2d99787e709323c91321caaf01c7297b
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-05-01 11:45:24 -0700

    work

--- .github/workflows/generate_report.py
+++ .github/workflows/generate_report.py
@@ -57,7 +57,11 @@ def check_requirements(parsed_files, requirements_file):
         if class_name in requirements:
             print("Class found: ")
             print(class_name)
+            print("Requirements found: ")
+            print(requirements[class_name])
             for method_name, line_count in methods.items():
+                print("Method found: ")
+                print(method_name)
                 if method_name in requirements[class_name]:
                     print("method in requirements: ")
                     print(method_name)
